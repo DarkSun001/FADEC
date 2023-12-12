738fba7ff8f29ceb1f49eae5446e232bdf7567dc
PERROUAS Thibault <107963463+Pyracantharia@users.noreply.github.com>
2023-12-12 11:52:57 +0100

    asset move
    j'ai bougé les assets

--- www/Views/Security/login.view.php
+++ www/Views/Security/login.view.php
@@ -9,15 +9,3 @@
     <button type="submit">Se Connecter</button>
 </form>
 <?php
-$host = 'postgres';
-$port = 5432;
-$database = 'fadec_bdd';
-$user = 'fadec_user';
-$password = 'fadec_mdp';
-
-try {
-    $dbh = new PDO("pgsql:host=$host;port=$port;dbname=$database;user=$user;password=$password");
-    echo "Connexion réussie !";
-} catch (PDOException $e) {
-    echo "Erreur de connexion : " . $e->getMessage();
-}
