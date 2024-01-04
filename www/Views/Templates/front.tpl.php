913418071c10d5860373905761d393cfb549c067
PERROUAS Thibault <thibaultperrouas8@gmail.com>
2024-01-04 17:57:41 +0100

    add route dash board

--- www/Views/Templates/front.tpl.php
+++ www/Views/Templates/front.tpl.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <title>Front</title>
-    </head>
-    <body>
-        <h1>Template du front</h1>
-        <?php include $this->view;?>
-    </body>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link href="https://cdn.jsdelivr.net/gh/Pyracantharia/CDN-FADEC@master/dist/main.css" rel="stylesheet">
+
+</head>
+
+
+<body>
+    <?php include $this->view;?>
+<script src="https://cdn.jsdelivr.net/gh/Pyracantharia/CDN-FADEC@master/src/js/main.js" type="module"></script>
+
+
+</body>
+
 </html>
