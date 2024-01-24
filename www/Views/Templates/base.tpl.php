bbb79f142dd3ab9536cfae28538e53e3ff0562c3
deathfal <56265997+deathfal@users.noreply.github.com>
2024-01-24 17:13:48 +0100

    create header & footer template
    Creating base template that include header and footer
    +
    add template to home page

---
+++ www/Views/Templates/base.tpl.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Back</title>
+    </head>
+    <body>
+        <?php include "header.tpl.php";?>
+
+        <?php include $this->view;?>
+
+        <?php include "footer.tpl.php";?>
+
+    </body>
+</html>
---
+++ www/Views/Templates/footer.tpl.php
@@ -0,0 +1,19 @@
+<footer class="footer">
+    <div class="container">
+        <div class="footer-content">
+            <div class="footer-section about">
+                <h2>À Propos</h2>
+                <p>Ce footer est juste un exemple pour illustrer.</p>
+                <div class="contact">
+                    <span><i class="fas fa-phone"></i> 123-456-789</span>
+                    <span><i class="fas fa-envelope"></i> info@example.com</span>
+                </div>
+                <div class="socials">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
