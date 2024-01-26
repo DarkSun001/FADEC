7ee09815ed8d7fc8d8685244402f6b69bd2d8c17
PERROUAS Thibault <thibaultperrouas8@gmail.com>
2024-01-26 16:20:01 +0100

    Ajout de lien vers le CDN-fadec
    cela permet d'appliquer le style

--- www/Views/Templates/base.tpl.php
+++ www/Views/Templates/base.tpl.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="UTF-8">
         <title>Back</title>
+        <script src="https://cdn.jsdelivr.net/gh/Pyracantharia/CDN-FADEC@master/src/js/main.js" type="module" defer></script>
+        <link href="https://cdn.jsdelivr.net/gh/Pyracantharia/CDN-FADEC@master/dist/main.css" rel="stylesheet">
     </head>
     <body>
         <?php include "header.tpl.php";?>
