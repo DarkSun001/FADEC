bf738cdcac4301a5169cce4c4bd8da8295ea899f
PERROUAS Thibault <107963463+Pyracantharia@users.noreply.github.com>
2024-02-20 16:06:07 +0100

    Update post.php

--- www/api/controllers/mail/post.php
+++ www/api/controllers/mail/post.php
@@ -11,16 +11,7 @@ try {
         throw new Exception("Invalid JSON input");
     }
 
-    $mail = new Mail();
-    $mail->recipient = $request["recipient"];
-    $mail->subject = $request["subject"];
-    $mail->message = $request["message"];
 
-    if ($mail->send()) {
-        Response::json(201, [], ["message" => "Mail sent", "mail" => $mail]);
-    } else {
-        Response::json(500, [], ["message" => "Mail not sent"]);
-    }
 } catch (Exception $e) {
     Response::json(500, [], ["message" => $e->getMessage()]);
 }
