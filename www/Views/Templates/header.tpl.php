32677b6ebdb8e3f6275db061929f98d3c510118a
PERROUAS Thibault <107963463+Pyracantharia@users.noreply.github.com>
2024-02-21 17:38:42 +0100

    redirect by no status = 3

--- www/Views/Templates/header.tpl.php
+++ www/Views/Templates/header.tpl.php
@@ -2,7 +2,7 @@
     <div class="container">
         <a href="#" class="navbar-brand">Logo</a>
         <ul class="navbar-nav">
-            <li><a href="#">Accueil</a></li>
+            <li><a href="/">Accueil</a></li>
             <li><a href="#">Services</a></li>
             <li><a href="#">À propos</a></li>
             <li><a href="#">Contact</a></li>
--- www/index.php
+++ www/index.php
@@ -21,21 +21,21 @@ function myAutoloader($class)
     //$class = App\Core\View
     $file = str_replace("App\\", "", $class);
     $file = str_replace("\\", "/", $file);
-    $file .=".php";
-    if(file_exists($file)){
+    $file .= ".php";
+    if (file_exists($file)) {
         include $file;
     }
 }
 
 $uri = strtolower($_SERVER["REQUEST_URI"]);
 $uri = strtok($uri, "?");
-if(strlen($uri )>1) $uri = rtrim($uri, "/");
+if (strlen($uri) > 1) $uri = rtrim($uri, "/");
 
 //Récupérer le contenu du fichier routes.yaml
 $fileRoute = "routes.yaml";
-if(file_exists($fileRoute)) {
+if (file_exists($fileRoute)) {
     $listOfRoutes = yaml_parse_file($fileRoute);
-}else{
+} else {
     die("Le fichier de routing n'existe pas");
 }
 
@@ -44,39 +44,76 @@ if(file_exists($fileRoute)) {
 //On fait toutes les vérifications nécessaires et on fait
 //une instance du controller et l'appel de l'action
 
-if(!empty($listOfRoutes[$uri])){
-    if(!empty($listOfRoutes[$uri]["controller"])){
-        if(!empty($listOfRoutes[$uri]["action"])){
+if (!empty($listOfRoutes[$uri])) {
+    if (!empty($listOfRoutes[$uri]["controller"])) {
+        if (!empty($listOfRoutes[$uri]["action"])) {
 
             $controller = $listOfRoutes[$uri]["controller"];
             $action = $listOfRoutes[$uri]["action"];
 
-            if(file_exists("Controllers/".$controller.".php")){
-                include "Controllers/".$controller.".php";
-                $controller = "App\\Controllers\\".$controller;
-                if(class_exists($controller)){
+            if (file_exists("Controllers/" . $controller . ".php")) {
+                include "Controllers/" . $controller . ".php";
+                $controller = "App\\Controllers\\" . $controller;
+                if (class_exists($controller)) {
                     $object = new $controller();
-                if(method_exists($object, $action)){
-                    $object->$action();
-                }else{
-                    die("L'action' ".$action. " n'existe pas");
+                    if (method_exists($object, $action)) {
+                        $object->$action();
+                    } else {
+                        die("L'action' " . $action . " n'existe pas");
+                    }
+                } else {
+                    die("Le class controller " . $controller . " n'existe pas");
                 }
-                }else{
-                    die("Le class controller ".$controller. " n'existe pas");
-                }
-            }else{
-                die("Le fichier controller ".$controller. " n'existe pas");
+            } else {
+                die("Le fichier controller " . $controller . " n'existe pas");
             }
-
-        }else{
-            die("La route ".$uri." ne possède pas d'action dans le ficher ".$fileRoute);
+        } else {
+            die("La route " . $uri . " ne possède pas d'action dans le ficher " . $fileRoute);
         }
-    }else{
-        die("La route ".$uri." ne possède pas de controller dans le ficher ".$fileRoute);
+    } else {
+        die("La route " . $uri . " ne possède pas de controller dans le ficher " . $fileRoute);
     }
-}else{
-//S'il n'y a pas de correspondance => page 404
+} else {
+    //S'il n'y a pas de correspondance => page 404
     include "Controllers/Error.php";
     $object = new Controllers\Error();
     $object->page404();
 }
+?>
+<script>
+   
+    var apiUrl = "";
+
+    // Envoi de la requête AJAX avec jQuery
+    $.ajax({
+        url: apiUrl,
+        type: 'GET',
+        contentType: 'application/json',
+        success: function(response) {
+            // La requête a fonctionné
+            console.log(response);
+
+            // Vérifier si aucun utilisateur avec le statut 3 n'est retourné
+            if (!(response.users && response.users.length > 0 && response.users[0].status === 3)) {
+                // Aucun utilisateur avec le statut 3, rediriger vers la page "register"
+                window.location.href = "register";
+                return; // Arrêter l'exécution ici pour éviter d'afficher le message
+            }
+
+            // Il y a des utilisateurs avec le statut 3, traiter normalement
+            $("#registerMessageContainer").html('<div class="text-green-600">' + response.message + '</div>');
+        },
+        error: function(error) {
+            // La requête n'a pas fonctionné
+            if (error.responseJSON) {
+                console.log(error.responseJSON.message); // Afficher le message d'erreur du serveur
+                // Afficher le message d'erreur dans la div registerMessageContainer
+                $("#registerMessageContainer").html('<div class="text-red-600">' + error.responseJSON.message + '</div>');
+            } else {
+                console.log("Erreur inattendue:", error.responseText);
+                // Afficher une erreur inattendue dans la div registerMessageContainer
+                $("#registerMessageContainer").html('<div class="text-red-600">Erreur inattendue: ' + error.responseText + '</div>');
+            }
+        }
+    });
+</script>
